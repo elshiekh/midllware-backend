a7c16c401e4a96a596f673d0430dc8dbc7b2730f
Omar Sadaqa <omarsadaqa@hotmail.com>
2020-12-08 14:46:12 +0300

    .

--- SFDA/Startup.cs
+++ SFDA/Startup.cs
@@ -36,10 +36,7 @@ namespace SFDA
             });
             services.RegsiterAPIMiddlewareConfiguration(Configuration);
 
-            services.Configure<IISServerOptions>(options =>
-            {
-                options.AllowSynchronousIO = true;
-            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
