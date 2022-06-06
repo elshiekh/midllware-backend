4a110945fd56e62a8407e708ef9fd31dd2ca58c2
Alshiekh <othman.ELhaikh@cloudsolutions.com.sa>
2022-06-06 08:46:01 +0300

    done

--- ERP.VIDA.In/Controllers/ERPController.cs
+++ ERP.VIDA.In/Controllers/ERPController.cs
@@ -145,29 +145,29 @@ namespace Vida.Controllers
         //}
         //#endregion
 
-        public void InsertQuestion(IEnumerable<string> area_list)
-        {
-            area_list.ToList().Add("TEST1");
-            area_list.ToList().Add("TEST2");
-            var connect = new OracleConnection("YOUR CONNECTION STRING");
+        //public void InsertQuestion(IEnumerable<string> area_list)
+        //{
+        //    area_list.ToList().Add("TEST1");
+        //    area_list.ToList().Add("TEST2");
+        //    var connect = new OracleConnection("YOUR CONNECTION STRING");
 
-            var command = new OracleCommand("BEGIN Testpackage.Testprocedure(:Areas); END;", connect);
+        //    var command = new OracleCommand("BEGIN Testpackage.Testprocedure(:Areas); END;", connect);
 
-            connect.Open();
+        //    connect.Open();
 
-            var arry = command.Parameters.Add("Areas", OracleDbType.Varchar2);
+        //    var arry = command.Parameters.Add("Areas", OracleDbType.Varchar2);
 
-            arry.Direction = ParameterDirection.Input;
-            arry.CollectionType = OracleCollectionType.PLSQLAssociativeArray;
-            arry.Value = area_list.ToArray();
-            arry.Size = area_list.Count();
-            arry.ArrayBindSize = area_list.Select(_ => _.Length).ToArray();
-            arry.ArrayBindStatus = Enumerable.Repeat(OracleParameterStatus.Success, area_list.Count()).ToArray();
+        //    arry.Direction = ParameterDirection.Input;
+        //    arry.CollectionType = OracleCollectionType.PLSQLAssociativeArray;
+        //    arry.Value = area_list.ToArray();
+        //    arry.Size = area_list.Count();
+        //    arry.ArrayBindSize = area_list.Select(_ => _.Length).ToArray();
+        //    arry.ArrayBindStatus = Enumerable.Repeat(OracleParameterStatus.Success, area_list.Count()).ToArray();
 
-            command.ExecuteNonQuery();
+        //    command.ExecuteNonQuery();
 
-            connect.Close();
-        }
+        //    connect.Close();
+        //}
 
         //#region Insert Booked Orders
         //[HttpPost("InsertBookedOrdersInToStg.{format}"), FormatFilter]
