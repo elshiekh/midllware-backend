16eca9fa06bf3b8de47fc5ca58c186fd0dd54e04
alshiekh <othman.elshaikh@cloudsolutions.com.sa>
2022-12-25 09:43:28 +0300

    last update

--- Electronic-Invoice-Out/Service/UserService.cs
+++ Electronic-Invoice-Out/Service/UserService.cs
@@ -17,10 +17,10 @@ namespace Electronic_Invoice_Out.Service
         // users hardcoded for simplicity, store in a db with hashed passwords in production applications
         private List<User> _users = new List<User>
         {
-            // DEV 
-            // new User { Id = 1, FirstName = "elevatusOut", LastName = "User", Username = "elevatusOut", HMGPassword = "mR79WX]FV" }
+           // DEV  
+           // new User { Id = 1, FirstName = "e-invoice-out", LastName = "User", Username = "e-Invoice", Password = "nfgYQbnyk" }
            // PROD 
-            new User { Id = 1, FirstName = "electronic-Invoice-out", LastName = "User", Username = "e-Invoice", Password = "nfgYQbnyk" }
+            new User { Id = 1, FirstName = "e-invoice-out", LastName = "User", Username = "e-Invoice", Password = "nbefSRmus" }
         };
 
         public async Task<User> Authenticate(string username, string password)
